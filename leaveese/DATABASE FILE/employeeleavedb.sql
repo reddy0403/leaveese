6be9354bf9309f8d1ea9aa04feb97448b4a681f6
reddy0403 <165401330+reddy0403@users.noreply.github.com>
2024-11-21 17:21:47 +0530

    Add files via upload

---
+++ leaveese/DATABASE FILE/employeeleavedb.sql
@@ -0,0 +1,61 @@
+
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `UserName` varchar(100) NOT NULL,
+  `Password` varchar(100) NOT NULL,
+  `fullname` varchar(255) NOT NULL,
+  `email` varchar(55) NOT NULL,
+  `updationDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+CREATE TABLE `tbldepartments` (
+  `id` int(11) NOT NULL,
+  `DepartmentName` varchar(150) DEFAULT NULL,
+  `DepartmentShortName` varchar(100) NOT NULL,
+  `DepartmentCode` varchar(50) DEFAULT NULL,
+  `CreationDate` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+CREATE TABLE `tblemployees` (
+  `id` int(11) NOT NULL,
+  `EmpId` varchar(100) NOT NULL,
+  `FirstName` varchar(150) NOT NULL,
+  `LastName` varchar(150) NOT NULL,
+  `EmailId` varchar(200) NOT NULL,
+  `Password` varchar(180) NOT NULL,
+  `Gender` varchar(100) NOT NULL,
+  `Dob` varchar(100) NOT NULL,
+  `Department` varchar(255) NOT NULL,
+  `Address` varchar(255) NOT NULL,
+  `City` varchar(200) NOT NULL,
+  `Country` varchar(150) NOT NULL,
+  `Phonenumber` char(11) NOT NULL,
+  `Status` int(1) NOT NULL,
+  `RegDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-
+CREATE TABLE `tblleaves` (
+  `id` int(11) NOT NULL,
+  `LeaveType` varchar(110) NOT NULL,
+  `ToDate` varchar(120) NOT NULL,
+  `FromDate` varchar(120) NOT NULL,
+  `Description` mediumtext NOT NULL,
+  `PostingDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `AdminRemark` mediumtext,
+  `AdminRemarkDate` varchar(120) DEFAULT NULL,
+  `Status` int(1) NOT NULL,
+  `IsRead` int(1) NOT NULL,
+  `empid` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-
